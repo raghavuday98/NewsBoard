c2057dbfe8a265bee7730a96c88b7666ce302edb
Uday <raghavudaypratap7@gmail.com>
2025-09-11 11:49:58 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
